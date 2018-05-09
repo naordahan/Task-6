98029827e1e00d977e41b0c31f5625c6c27113e6
annty2 <33619352+annty2@users.noreply.github.com>
2018-05-09 18:43:53 +0300

    Update Node.cpp

--- Node.cpp
+++ Node.cpp
@@ -1,1 +1,59 @@
 
+#include "Board.h"
+
+//constructors
+Node::Node(){}
+
+Node::Node(char d)
+{
+    if(d=='X' || d== 'O' || d=='.')
+        this->data = d;
+    else{
+        IllegalCharException e;
+        e.setInput(data);
+        throw e;
+    }
+}
+Node::Node(const Node& n)
+{
+    this->data = n.getNode();
+}
+
+
+char Node::getNode() const
+{
+    return this->data;
+}
+
+Node& Node::operator=(char d)
+{
+   if(d=='X' || d=='O' || d=='.'){
+        this->data = d;
+        return *this;
+   }
+    else
+    {
+        IllegalCharException e;
+        ex.setInput(data);
+        throw e;
+    }
+    return *this;
+}
+
+bool Node::operator==(char c) const
+{
+    return data == c;
+}
+
+bool Node::operator==(const Node& o) const
+{
+    return data== o.data;
+}
+
+bool Node::operator!=(char c) const {
+    return data != c;
+}
+
+bool Node::operator!=(const Node& n) const {
+    return data!= o.data;
+}
