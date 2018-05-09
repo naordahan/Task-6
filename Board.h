9a92e1a8e0f73af1343835929fd6578c6e5986bc
annty2 <33619352+annty2@users.noreply.github.com>
2018-05-09 18:46:38 +0300

    Update Board.h

--- Board.h
+++ Board.h
@@ -1,24 +1,52 @@
 #pragma once
 #include <iostream>
-#include <string>
-#include "Coordinate.hpp"
-
-
+#include <exception>
+#include <list>
+#include "Node.h"
 using namespace std;
 
-class Board {
-	int size;
-	char  **board;
-public:
-	Board();
-	Board(int board_size);
-	Board& operator=(char c);
-	Board(const Board &Board);
-	Board& operator[](Coordinate cor);
-	Board& operator= (Board &Board1);
-	friend ostream& operator<< ( ostream& os, Board &Board);
-
+class Board
+{
+    public:
+    int size;
+    Node **board;
+    Board(int s);
+    Board(const Board& o);
+    ~Board();
+    bool operator==(const Board& o) const;
+    Node& operator[](list<int> list);
+    Board& operator=(const Board& b);
+    Board& operator=(char c);
+    friend ostream &operator<<(ostream &o, const Board &b);
 };
 
+//exceptions
+class IllegalCoordinateException : public exception
+{
+    int x,y;
+    public: 
+        void setX(int row){
+            this->x=row;
+            }
+        void setY(int col){
+            this->y=col;
+            }
+        string theCoordinate ()const
+        {
+            return to_string(x)+","+to_string(y);
+        }
+};
 
-
+class IllegalCharException : public exception
+{
+    char in;
+    
+    public:
+        char theChar()const
+        {
+            return in;
+        }
+        void setInput(int n){
+            in=n;
+        }
+};
