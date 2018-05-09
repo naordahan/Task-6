d62df12206fc341c08f6e0e9a9576b2742bbcc09
annty2 <33619352+annty2@users.noreply.github.com>
2018-05-09 18:42:11 +0300

    Update Node.h

--- Node.h
+++ Node.h
@@ -1,1 +1,26 @@
+#include <iostream>
+using namespace std;
 
+class Node{
+  
+    char data;
+  
+ public:
+    Node();
+    Node(char d);
+    Node(const Node& n);
+    char getNode() const;
+    Node& operator=(char d);
+    bool operator==(char c) const;
+    bool operator==(const Node& o) const;
+    bool operator!=(char c) const;
+    bool operator!=(const Node& n) const;
+    friend ostream& operator<< (ostream& os, Node& n);
+};
+
+//operator <<
+inline ostream& operator<< (ostream& os, Node& n)
+{
+    os << n.getNode();
+    return os;
+}
