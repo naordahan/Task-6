d81ce86b0c84e73f7488a2001781b45ff22a9480
naordahan <33223417+naordahan@users.noreply.github.com>
2018-05-09 21:21:03 +0300

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -3,7 +3,7 @@
 Board::Board(int s)
 {
     this->size=s;
-    this->game = new Node*[size];
+    this->board = new Node*[size];
     for (int i = 0; i < size; i++) {
 		this->board[i] = new Node [size];
 	}
