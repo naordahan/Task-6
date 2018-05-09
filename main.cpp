e1f2f3e51689e56bb4c54beae45774f59feeb9fc
naordahan <33223417+naordahan@users.noreply.github.com>
2018-05-09 20:38:43 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,12 +6,7 @@ using namespace std;
 
 int main() {
 	Board board1{4};  // Initializes a 4x4 board
-	cout << board1 << endl;   /* Shows an empty board:
-	....
-	....
-	....
-	....
-	*/
+	cout << board1 << endl;  
 	cout << board1[{1,2}] << endl; // .
 	board1[{1,1}]='X';
 	board1[{1,2}]='O';
