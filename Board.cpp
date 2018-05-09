3de7ea45500e8513cdc2a1878627c0b14e995311
naordahan <33223417+naordahan@users.noreply.github.com>
2018-05-09 19:42:12 +0300

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -48,7 +48,7 @@ Node& Board::operator[](list<int> list)
     {
        IllegalCoordinateException ex;
         ex.setX(x); ex.setY(y);
-        throw ee;
+        throw ex;
     }   
     return board[x][y];
 }
